2b511d4f30bf2c580b63d2fee61830223f6947be
JekenMaharjan <maharjanjeken@gmail.com>
2026-04-13 10:54:34 +0545

    feat: add main index page

--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('hello_world', views.hello_world),
     path('HelloEthiopia', views.HelloEthiopia.as_view()),
     path('my_first', views.my_first),
