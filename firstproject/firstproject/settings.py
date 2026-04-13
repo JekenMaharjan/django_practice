ae3825afc6087a719af493f029a835dbadd44177
JekenMaharjan <maharjanjeken@gmail.com>
2026-04-13 11:13:39 +0545

    feat: 404 - page not found error

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iind!*&-1_5sb&h)uws3$$=fhi@s_t-k-fllk)#*i+@l@f76b2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
